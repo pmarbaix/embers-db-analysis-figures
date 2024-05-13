b61aa6aae4780b9034cb68b05446cc4dcad73c71
marbaix <philippe.marbaix@uclouvain.be>
2024-05-13 14:41:18 +0200

    Remove token (will be canceled and replaced by a confidential one)

--- settings_data_access.py
+++ settings_data_access.py
@@ -1,12 +1,12 @@
-source = "local"
+source = "remote"
 
 if source == "local":
     API_URL = "http://localhost:8000"
-    TOKEN = "d4ec10cb9d197550b246bcc48760f8cd6b6d25e6"
+    TOKEN = "the-token-that-was-here-is-canceled"
     print("WARNING: getting data from the LOCAL database")
 elif source == "remote":
     API_URL = "https://climrisk.org/edb1B"
-    TOKEN = "d4ec10cb9d197550b246bcc48760f8cd6b6d25e6"
+    TOKEN = "the-token-that-was-here-is-canceled"
 elif source == "file":
     pass
 else:
